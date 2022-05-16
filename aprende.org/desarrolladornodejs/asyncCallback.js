966362c4026c316d2e2a61f40e4a5a7f1cd9a598
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2022-05-16 16:10:24 -0500

    Ejercicios 3 y 4 de la Leccion 2, Nivel 2.

---
+++ aprende.org/desarrolladornodejs/asyncCallback.js
@@ -0,0 +1,9 @@
+function realizaAlgo(asyncCallback) {
+    setTimeout(asyncCallback, Math.random()+1000);
+}
+
+realizaAlgo(()=> {
+    console.log("Esto se ejecuta de forma asincrona")
+});
+
+console.log("Respuesta adelantada")
---
+++ aprende.org/desarrolladornodejs/i_o_files.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+
+fs.writeFile('mensaje.txt', 'Esto es un mensaje', function() {console.log('Guardado');});
+console.log("Escribiendo en archivo...");
+
+fs.readFile('mensaje.txt', function(err,datos) {console.log(datos);});
+
+fs.readFile('mensaje.txt', {encoding: 'utf-8'}, function(err,datos) {console.log(datos);});
