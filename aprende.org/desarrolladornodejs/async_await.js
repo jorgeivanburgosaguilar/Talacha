f216d92df1fb4e5142ac4525920e3a5247968da4
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2022-05-20 09:48:05 -0500

    Ejercicios Nivel 2, Leccion 2 4 y 5. Funcion Promise y Async Await.

---
+++ aprende.org/desarrolladornodejs/async_await.js
@@ -0,0 +1,20 @@
+function tripleDespues1Segundo(numero) {
+    return new Promise(resolve => {
+        setTimeout(()=> {
+           resolve(numero*3);
+        }, 1000);
+    });
+}
+
+const ResultadoFinal = async function (numero) {
+    try {
+        let triple = await tripleDespues1Segundo(numero).then((result)=> {
+            console.log(result);
+        });
+        return triple;
+    } catch (error) {
+        console.log("ERROR =>", error);
+    }
+}
+
+ResultadoFinal(20);
---
+++ aprende.org/desarrolladornodejs/promise-rechazada.js
@@ -0,0 +1,20 @@
+let promesa = new Promise(function (resolve, reject){
+    setTimeout(() => {
+        resolve("Hecho"); // Cambia el estado a 'cumplido'
+    }, 3000);
+});
+
+console.log('Promesa antes de ser resuelta', promesa); // imprimme la promesa para ver el estado
+setTimeout(()=> { console.log('Promesa antes de ser resuelta', promesa)}, 3000);
+// ejecuta la misma funcion de tiempo para el mensaje en la consola para ver claramente el resultado
+
+
+let promesaRechazada = new Promise(function (resolve, reject){
+    setTimeout(() => {
+        reject("ERROR!!!"); // Cambia el estado a 'cumplido'
+    }, 3000);
+});
+
+console.log('Promesa antes de ser rechazada', promesaRechazada); // imprimme la promesa para ver el estado
+setTimeout(()=> { console.log('Promesa antes de ser rechazada', promesaRechazada)}, 3000);
+// ejecuta la misma funcion de tiempo para el mensaje en la consola para ver claramente el resultado
