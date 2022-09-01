6a91c339759c018565bc69d435f9d120d4f49dec
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2022-09-01 16:16:54 -0500

    leetcode 11

---
+++ LeetCode/11. Container With Most Water.cs
@@ -0,0 +1,32 @@
+/*
+11. Container With Most Water
+Runtime: 196 ms, faster than 89.68% of C# online submissions for Container With Most Water.
+Memory Usage: 50.3 MB, less than 15.18% of C# online submissions for Container With Most Water.
+*/
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        var indexAdd = 0;
+        var indexSubstract = height.Length - 1;
+        
+        var maxArea = 0;
+        while (indexAdd < indexSubstract)
+        {
+            var heightIndexAdd = height[indexAdd];
+            var heightIndexSubstract = height[indexSubstract];
+            
+            maxArea = Math.Max((indexSubstract - indexAdd) * Math.Min(heightIndexAdd, heightIndexSubstract), maxArea);
+            if (heightIndexAdd > heightIndexSubstract)
+            {
+                indexSubstract--;
+            }
+            else
+            {
+                indexAdd++;
+            }
+        }
+        
+        return maxArea;
+    }
+}
