09aba8d36b972777c53de518c961f20332660304
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2022-09-29 16:14:08 -0500

    reboot angular-tutorial 14.2

--- angular-tutorial/src/app/app.module.ts
+++ angular-tutorial/src/app/app.module.ts
@@ -1,28 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { HelloWorldTemplateComponent } from './hello-world-template.component';
-import { HelloWorldNgIfComponent } from './hello-world-ngif/hello-world-ngif.component';
-import { HelloWorldDependencyInjectionComponent } from './hello-world-di/hello-world-di.component';
-import { HelloWorldInterpolationComponent } from './hello-world-interpolation/hello-world-interpolation.component';
-import { HelloWorldBindingsComponent } from './hello-world-bindings/hello-world-bindings.component';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent,
-    HelloWorldTemplateComponent,
-    HelloWorldNgIfComponent,
-    HelloWorldDependencyInjectionComponent,
-    HelloWorldInterpolationComponent,
-    HelloWorldBindingsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
