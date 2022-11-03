655c215ca5207d50a4895f8c31dd854fee5de3c1
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2022-11-03 16:18:34 -0600

    parte 1 quitar esbuild (me rindo simplmente demasiado legacy usar jquery con npm sin webpack)

--- Triple I/Frontend-Hugo-jQuery-Bootstrap/assets/js/index.js
+++ Triple I/Frontend-Hugo-jQuery-Bootstrap/assets/js/index.js
@@ -3,4 +3,5 @@ import "datatables.net";
 import "datatables.net-bs5";
 import "datatables.net-buttons-bs5";
 
-window.DataTable = setup(window, $);
+window.$ = window.jQuery = $;
+//window.DataTable = setup(window, $);
