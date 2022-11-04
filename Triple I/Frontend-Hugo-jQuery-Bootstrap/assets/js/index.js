9a7ebdb1b420b2788f0b434878bc7d7bcf6b4a4c
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2022-11-04 14:39:28 -0600

    parte 2 y fin de quitar soporte para esbuild en favor de solo usar el bundler de hugo, queda mas complicado pero funciona.

--- Triple I/Frontend-Hugo-jQuery-Bootstrap/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-import "datatables.net";
-import "datatables.net-bs5";
-import "datatables.net-buttons-bs5";
-
-window.$ = window.jQuery = $;
-//window.DataTable = setup(window, $);
