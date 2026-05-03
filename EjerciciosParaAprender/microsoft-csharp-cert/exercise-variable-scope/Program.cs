b6eba49e1c163ca458906d1df40d628aa01ce687
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2026-05-03 00:49:45 -0600

    ejercicios de c# y actualizar proyecto para desencriptar membership api

---
+++ EjerciciosParaAprender/microsoft-csharp-cert/exercise-variable-scope/Program.cs
@@ -0,0 +1,37 @@
+﻿bool flag = true;
+int value = 10;
+
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
+
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve")
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+    {
+        bool found = true;
+        if (found)
+        {
+            Console.WriteLine("Set contains 42");
+        }
+    }
+}
+
+Console.WriteLine($"Total: {total}");
